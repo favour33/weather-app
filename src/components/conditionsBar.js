15d74f3afc683f1e93541f069460cb24b6e7eeb9
Favour Nwachukwu <fnwachukwu84@gmail.com>
2022-03-09 11:34:09 +0000

    Add Modular Design Pattern and Extra CSS to closer match the design

--- src/components/conditionsBar.js
+++ src/components/conditionsBar.js
@@ -1,34 +1,60 @@
 import React from "react";
-import feelsLikePic from '../pic/feelsLike.PNG'
-import windPic from '../pic/windy.PNG'
-import humidityPic from '../pic/partRain.PNG'
-
+import feelsLikePic from "../pic/feelsLike.PNG";
+import windPic from "../pic/windy.PNG";
+import humidityPic from "../pic/partRain.PNG";
 
 const ConditionsBar = () => {
-    return (
-        <div class="conditionsBarContainer">
-            <div id="condition1">
-                Feels Like
-                <div id="thermostatImgContainer">
-                    <img id="thermometerIcon" src={feelsLikePic} alt="icon" />
-                </div>
-                <div id="conditionsImgPic1" class="conditionsImgPic">3°</div>
-            </div>
-            <div id="condition2">
-                Wind
-                <div id="windyImgContainer">
-                    <img id="windyIcon" src={windPic} alt="windy" />
-                </div>
-                <div id="conditionsImgPic2" class="conditionsImgPic">4.1 mph</div>
-            </div>
-            <div id="condition3">
-                Humidity
-                <div id="humidityImgContainer">
-                    <img id="humidityIcon" src={humidityPic} alt="humidity" />
-                </div>
-                <div id="conditionsImgPic3" class="conditionsImgPic">87%</div>
-            </div>
-        </div >
-    )
-}
-export default ConditionsBar
+  return (
+    <div class="conditionsBarContainer">
+      <div className="condition">
+        <p className="condition-name">Feels Like</p>
+        <img id="thermometerIcon" src={feelsLikePic} alt="Themostat" />
+        <p className="degree">3°</p>
+      </div>
+      {/*  */}
+      <div className="condition">
+        <p className="condition-name">Wind</p>
+        <img id="windyIcon" src={windPic} alt="windy" />
+        <p className="degree">4.1 mph</p>
+        {/*  */}
+      </div>
+      {/*  */}
+      <div className="condition">
+        <p className="condition-name">Humidity</p>
+        <img id="humidityIcon" src={humidityPic} alt="humidity" />
+        <p className="degree">87%</p>
+        {/*  */}
+      </div>
+
+      {/* <div id="condition1">
+        Feels Like
+        <div id="thermostatImgContainer">
+          <img id="thermometerIcon" src={feelsLikePic} alt="icon" />
+        </div>
+        <div id="conditionsImgPic1" class="conditionsImgPic">
+          3°
+        </div>
+      </div> */}
+
+      {/* <div id="condition2">
+        Wind
+        <div id="windyImgContainer">
+          <img id="windyIcon" src={windPic} alt="windy" />
+        </div>
+        <div id="conditionsImgPic2" class="conditionsImgPic">
+          4.1 mph
+        </div>
+      </div> */}
+      {/* <div id="condition3">
+        Humidity
+        <div id="humidityImgContainer">
+          <img id="humidityIcon" src={humidityPic} alt="humidity" />
+        </div>
+        <div id="conditionsImgPic3" class="conditionsImgPic">
+          87%
+        </div>
+      </div> */}
+    </div>
+  );
+};
+export default ConditionsBar;
--- src/components/topBar.js
+++ src/components/topBar.js
@@ -1,23 +1,19 @@
-import React from 'react'
-import menuImg from '../pic/sidebar2.png'
+import React from "react";
+import menuImg from "../pic/sidebar2.png";
 
 const TopBar = () => {
-    return (
-        <div class='topContainer'>
-            <div id="conditions">
-                Full Moon Night
-            </div>
-            <div id="location">
-                London, UK
-                <div id="temperature">
-                    4°
-                </div>
-            </div>
-            <div id="menuImg">
-                <img id="menueImage" src={menuImg} alt="MenuIcon" />
-            </div>
-        </div >
-    )
-}
+  return (
+    <div class="topContainer">
+      <div id="conditions">Full Moon Night</div>
+      <div id="location">
+        London, UK
+        <div id="temperature">4°</div>
+      </div>
+      <div id="menuImg">
+        <img id="menueImage" src={menuImg} alt="MenuIcon" />
+      </div>
+    </div>
+  );
+};
 
-export default TopBar
+export default TopBar;
