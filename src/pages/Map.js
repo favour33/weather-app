4dead5f937d2726791b5ab61dbc00312a1106eae
franci301 <gregotti20@gmail.com>
2022-03-20 15:37:36 +0000

    added new api for the map

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,34 +1,42 @@
 import React, {useEffect, useState} from "react";
-
+import AerisWeather from '@aerisweather/javascript-sdk';
+import '@aerisweather/javascript-sdk/dist/styles/styles.css';
 
 const Map = () => {
 
   useEffect(() => {
-    getWeatherMap();
+    const aeris = new AerisWeather('S7qAPDYfude8398481Psc', 'RXjWtivCWg69bT0WeuSBcaW2TS1rhewSpmQ3ltKJ');
+      
+
+        aeris.views().then((views) => {
+            const map = new views.Map('#mapview', {
+                map: {
+                    layers: {
+                        data: 'alerts,radar'
+                    },
+                    center: 'austin,tx',
+                    zoom: 6,
+                    size: {
+                        width: 800,
+                        height: 600
+                    }
+                }
+            })
+        });
   }, []);
   
-  const [map, setMap] = useState({});
-  const REACT_APP_API_KEY = "89e4bc1fe6d1e59b2d9cad9147c51e5e";
 
 
-  const getWeatherMap = async () => {
-    const response = await fetch(
-      `https://tile.openweathermap.org/map/temp_new/1/0/1.png?appid=${REACT_APP_API_KEY}`
-    );
 
-    const imageBlob = await response.blob();
-    const imageObjectURL = URL.createObjectURL(imageBlob);
-    setMap(imageObjectURL);
-  };
+ 
 
   
 
   return(
     <div>
-      <link rel="stylesheet"  href="//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.2/leaflet.css"/>
       Map
-      <div>
-        <img src={map} style={{width:"340px",height:"620px"}} ></img>
+      <div id="mapview" style={{width:"340px",height:"620px"}}>
+       
       </div>
     </div>
   );
